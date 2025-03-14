4e37d311a3cbbd326f942117cc49e29604fd5d4c
Your Name <your.email@example.com>
2025-03-14 02:52:16 +0200

    Citirea unui numar intreg

--- Seminar01.c
+++ Seminar01.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 int main() {
-	printf("Salutare!");
+	printf("Salutare!\n Introduceti numarul intreg:");
+	int variabila=0;
+	scanf_s("%d", &variabila);
+	printf("Ai introdus: %d", variabila);
 	return 0;
 }
